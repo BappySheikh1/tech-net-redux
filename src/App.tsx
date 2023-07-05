c0d2a78a35708046fb0eea7c3ba3d2a0f6bcfe68
BappySheikh1 <sheikhbappy91gmail.com>
2023-07-05 21:46:16 +0600

    redux slice added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { RootState } from "./redux/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const { count } = useAppSelector((state: RootState) => state.counter);
+  const dispatch = useAppDispatch();
+
   return (
     <>
-      <h1 className="text-5xl font-bold text-center">Tech Net</h1>
+      <div className="flex items-center">
+        <button
+          className=" border border-green-700 btn btn-outline p-2"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+
+        <button
+          className=" border border-green-700 btn btn-outline p-2"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          Increment By Value
+        </button>
+        <div className="mx-2">{count}</div>
+        <button
+          className="p-2 border border-red-700"
+          onClick={() => dispatch(decrement())}
+        >
+          decrement
+        </button>
+      </div>
     </>
   );
 }
